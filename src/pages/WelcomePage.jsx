e68e2a93c3688f8eeff7255623dd49d37b03c6a8
Osagie Amadasun <osagieamadasun123@gmail.com>
2025-01-26 02:52:52 +0100

    added animation to the homepage

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -5,7 +5,7 @@ function WelcomePage() {
   const navigate = useNavigate();
 
   return (
-    <div className='h-screen'>
+    <div className='min-h-screen'>
       {/* Responsive Navigation Bar */}
       <nav className='flex justify-between items-center mb-32 p-4 md:p-6 lg:p-8'>
         <h1 className='font-black text-2xl sm:text-3xl md:text-4xl text-white cursor-pointer'>Blogger</h1>
@@ -20,7 +20,7 @@ function WelcomePage() {
       </nav>
 
       {/* Responsive Main Content */}
-      <main className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl mt-10 sm:mt-20 flex justify-center items-center text-left'>
+      <main className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl mt-10 sm:mt-20 flex justify-center items-center text-left animate-fade-in'>
         <div className='container p-4 sm:p-8 md:p-12 lg:p-16'>
           <p className='mx-4 sm:mx-10  md:mx-20 lg:mx-40 mb-0 font-extrabold text-white text-4xl text-center sm:text-5xl md:text-6xl lg:text-7xl'>
             Write, <span className='text-cyan-600'>Design,</span> <span className='text-amber-800'>Publish...</span> Seamless Blogging Made <span className='font-Allura'>Simple!</span>
